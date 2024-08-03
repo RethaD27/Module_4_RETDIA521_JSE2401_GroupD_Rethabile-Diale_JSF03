076db214c46cf8e35e3c30e04868d351cbb9f8ac
Rethabile Diale <rethadiale27@gmail.com>
2024-08-03 08:17:21 +0200

    Added Loading component for indicating data fetching state

---
+++ src/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loading text-center text-2xl justify-center items-center">
+    Loading...
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
